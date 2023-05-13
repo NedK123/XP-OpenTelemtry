9ff2de089b576d9fd8815e1308064bf39e5f95e5
naderkahwaji <nader.kahwaji@gmail.com>
2023-05-13 19:06:36 +0300

    first commit

---
+++ src/main/java/com/example/xpopentelemetry/XpOpenTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpopentelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpOpenTelemetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpOpenTelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xpopentelemetry/XpOpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpopentelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpOpenTelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
